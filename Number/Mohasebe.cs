37261f159de5b0a1dab3bb0de40d8d93c094a5a9
Rooman <Rooman@ROOMAN>
2023-10-09 00:25:21 +0330

    Add project files.

---
+++ Number/Mohasebe.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    internal class Mohasebe
+    {
+        public void AddAval()
+        {
+            Console.WriteLine("Plese Enter Number(<=0):");
+            Console.Write("AZ: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("TA:");
+            int M = int.Parse(Console.ReadLine());
+            if (M < N)
+            {
+                Console.WriteLine("Error M<N");
+                return;
+            }
+            for (int X = N; X < M; X++)
+            {
+                bool Pirme = true;
+                if (X < 2)  Pirme  = false;
+                for (long i = 2; i*i <= X; i++)
+                {
+                   
+                    if (X % i == 0)
+                    {
+                        Pirme = false;
+                        break;
+                    }
+                }
+                if (Pirme) { Console.WriteLine("("+" "+ X + " " +")"); }
+            }
+            Console.ReadKey();
+        }
+        public void AddAynei() 
+        {
+            Console.WriteLine("Plese Enter Number(<=0):");
+            Console.Write("AZ: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("TA:");
+            int M = int.Parse(Console.ReadLine());
+            if (M < N)
+            {
+                Console.WriteLine("Error M<N");
+                return;
+            }
+            for (int Y = N; Y <= M; Y++)
+            {
+                int sum = 0;
+                int n = Y;
+                int R;
+                int temp = n;
+                while (n > 0)
+                {
+                    R = n % 10;
+                    sum = (sum * 10) + R;
+                    n = n / 10;
+                }
+                if (temp == sum)
+                {
+                    Console.WriteLine("(" + " " + Y + " " + ")");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using Number;
+
+Mohasebe mohasebe = new Mohasebe();
+Console.WriteLine("Welcome");
+Console.WriteLine("Information App: Daryaft Add Va Tabdil Be Aval Ya Aynei ");
+Console.WriteLine("*************************************************************************");
+Console.WriteLine("1: Add Aval");
+Console.WriteLine("2: Add Aynei");
+Console.WriteLine("*************************************************************************");
+Console.Write("Pealse Enter Number (1/2):");
+int quiz = int.Parse(Console.ReadLine());
+
+if (quiz == 1)
+{
+    Console.WriteLine("Adad Aval:");
+    mohasebe.AddAval();
+}
+else if (quiz == 2)
+{
+    mohasebe.AddAynei();
+}
+else 
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+
